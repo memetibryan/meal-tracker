fe6a099f9c5fe01300928bbea02483d8497b711b
memetibryan <memetibryan@gmail.com>
2017-06-02 10:41:36 +0300

    example angular code to-do-list

---
+++ app/task-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from './task.model';
+
+@Component({
+  selector: 'task-list',
+  template: `
+  <select (change)="onChange($event.target.value)">
+  <option value="all">Show All</option>
+  <option value="isDone">Show Done</option>
+  <option value="notDone" selected="selected">Show Not Done</option>
+</select>
+    <div *ngFor="let currentTask of childTaskList | completeness:selectedCompleteness">
+      <h3>{{ currentTask.description }}</h3>
+      <button (click)="editButtonHasBeenClicked(currentTask)">Edit</button>
+    </div>
+  `
+})
+
+export class TaskListComponent {
+  public selectedCompleteness: string = "notDone";
+  onChange(optionFromMenu) {
+  this.selectedCompleteness = optionFromMenu;
+  console.log(this.selectedCompleteness);
+}
+  @Input() childTaskList: Task[];
+  @Output() clickSender = new EventEmitter();
+  editButtonHasBeenClicked(taskToEdit: Task) {
+    this.clickSender.emit(taskToEdit);
+  }
+}
