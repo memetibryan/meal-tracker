2a303f777e30ff567f03deea3dac6ab23bcb05d5
memetibryan <memetibryan@gmail.com>
2017-06-05 09:36:10 +0300

    editted app.components.js food lists

--- app/app.component.ts
+++ app/app.component.ts
@@ -32,13 +32,7 @@ import { Food } from './task.model';
 
 export class AppComponent {
   public masterTaskList:Food[] = [
-      new Food("Rice and Beans.",320, "Best meal ever with my mum after church"),
-      new Food("Tea with Bread.",150, "Normal,though noticed it had a lot of sugar"),
-      new Food("Pilau with yoghurt.",215, "Yummy! There's nothing sweet like this...!"),
-      new Food("Ugali with Fish.",180, "Kind liked the meal though i hate fish because of  the many bones...haha, ikr!"),
-      new Food("Chapati with Ndengu.",340, "Sound fancy i know but its normal and cheap hence convenient as i hardly had no money that day"),
-      new Food("Pizza with Soda.",90, "Wow,The best veges pizza i ever had, I swear i will go back to the place"),
-      new Food("Fish with Ugali.",103, "No say here!")
+      new Food()
   ];
   selectedTask: Food = null;
   showDetails(clickedTask: Food) {
